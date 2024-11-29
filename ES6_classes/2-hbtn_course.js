10d1457f89488a788273b4250025cfc4a2b2a495
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-11-29 00:28:44 -0500

    3

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -4,37 +4,37 @@ export default class HolbertonCourse {
 	  this.length = length;
 	  this.students = students;
 	}
-  
+
 	get name() {
 	  return this._name;
 	}
-  
+
 	set name(Newname) {
 	  if (typeof Newname !== 'string') {
 		throw new TypeError('Name must be a string');
 	  }
 	  this._name = Newname;
 	}
-  
+
 	get length() {
 	  return this._length;
 	}
-  
+
 	set length(Newlength) {
 	  if (typeof Newlength !== 'number') {
 		throw new TypeError('Length must be a number');
 	  }
 	  this._length = Newlength;
 	}
-  
+
 	get students() {
 	  return this._students;
 	}
-  
+
 	set students(Newstudent) {
 	  if (!Array.isArray(Newstudent) || !Newstudent.every((student) => typeof student === 'string')) {
 		throw new TypeError('Students must be an array of strings');
 	  }
 	  this._students = Newstudent;
 	}
-  }
+}
