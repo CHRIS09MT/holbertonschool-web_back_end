adb4c10d265ae8b63194815d1313b2a285a412f2
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-12-04 16:21:55 -0500

    add a typing

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
+from typing import List
+
 """
 Module
 """
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
-    List type float
+    Variable with a list type float
     """
     return sum(input_list)
