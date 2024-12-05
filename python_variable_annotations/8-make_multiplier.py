bae93197e5377f3e976dc7e1dd40de91cd95fcdb
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-12-05 10:00:28 -0500

    return a function

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -5,12 +5,16 @@ Module
 
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
     Multiplier function
     """
-    
+
     def multiplier_function(value: float) -> float:
+        """
+        Return the multiplier function
+        """
         return value * multiplier
-    
+
     return multiplier_function
