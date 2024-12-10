27da841e591d8aa10e1c781dae04f155f2d1f7f4
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-12-10 12:10:38 -0500

    eliminated a variable

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -4,11 +4,12 @@ Module
 """
 
 import asyncio
+from asyncio import Task
 
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-def task_wait_random(max_delay: int):
+def task_wait_random(max_delay: int) -> Task:
     """
     ...
     """
