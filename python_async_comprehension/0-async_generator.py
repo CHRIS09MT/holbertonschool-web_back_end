6aab83b37c32678c02ad6ff09449084209feefc4
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-12-12 17:52:40 -0500

    Yes

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module
+"""
+
+import random
+import asyncio
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    ...
+    """
+
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
