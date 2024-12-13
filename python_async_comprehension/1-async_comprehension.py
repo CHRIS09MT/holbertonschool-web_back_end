cd8b3065e6a785bb039b36ce0886cdf161d4ac1f
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-12-13 15:37:46 -0500

    add lines

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -10,5 +10,8 @@ async_generator = __import__("0-async_generator").async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """..."""
+    """
+    ...
+    """
+
     return [num async for num in async_generator()]
