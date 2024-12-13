e1662feca6a09f22fa5ec6477335f539b1c8894a
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-12-12 20:23:37 -0500

    ready?

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,13 +4,15 @@ Module
 """
 
 import random
+import asyncio
 from typing import Generator
 
 
-def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     ...
     """
 
     for _ in range(10):
+        await asyncio.sleep(1)
         yield random.uniform(0, 10)
