24ba4e256f7443ada8830b0a8076950c9aea1073
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-12-17 09:08:34 -0500

    pagination

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Pagination
+    """
+
+    start: int = (page - 1) * page_size
+    end: int = start + page_size
+    return (start, end)
