557d011153c563fb9077fd5cd4ec995fc6c19f39
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-12-19 13:43:02 -0500

    READY

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -3,11 +3,11 @@
 Module
 """
 
-index_range = __import__("0-simple_helper_function").index_range
-
 import csv
 import math
 from typing import List
+
+index_range = __import__("0-simple_helper_function").index_range
 
 
 class Server:
