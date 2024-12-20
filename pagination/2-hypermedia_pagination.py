52bab0976efbf309663f315a036bdb4256ca9c7c
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-12-20 14:18:28 -0500

    modify the file

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -45,7 +45,7 @@ class Server:
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         """
-        Return
+        Return a dictionary with pagination information.
         """
 
         data = self.get_page(page, page_size)
@@ -70,7 +70,7 @@ class Server:
             "page_size": len(data),
             "page": page,
             "data": data,
-            "next_page": page + 1 if page < total_pages else None,
-            "next_page": page - 1 if page > 1 else None,
+            "next_page": next_page,
+            "prev_page": prev_page,
             "total_pages": total_pages,
         }
