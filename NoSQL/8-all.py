00684083b4ef25bbc65046fb1d9776aa80ded35a
CHRIS09MT <christophermoralestorres06@gmail.com>
2025-01-07 20:14:49 -0500

    listing all document

---
+++ NoSQL/8-all.py
@@ -0,0 +1,8 @@
+def list_all(mongo_collection: list) -> list:
+    """
+    listing a collection
+    """
+
+    if mongo_collection is None:
+        return []
+    return list(mongo_collection.find())
