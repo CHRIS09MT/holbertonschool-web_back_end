92132ad54734513f27bbd26980a18673672c52d8
CHRIS09MT <christophermoralestorres06@gmail.com>
2025-01-22 14:04:48 -0500

    end

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,5 +5,32 @@ Module
 
 from pymongo import MongoClient
 
+
+def logs(n: int):
+    """
+    Logs status
+    """
+
+    client = MongoClient("mongodb://localhost:27017/")
+
+    logs = client.logs.nginx
+
+    return logs.count_documents(n)
+
+
 def main():
-    pass
+    """
+    The Main
+    """
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    print(f"{logs({})} logs")
+    print("Methods:")
+    for method in methods:
+        print(f"\tmethod {method}: {logs({'method': method})}")
+    print(f"{logs({'method': 'GET', 'path': '/status'})} status check")
+
+
+if __name__ == "__main__":
+    main()
