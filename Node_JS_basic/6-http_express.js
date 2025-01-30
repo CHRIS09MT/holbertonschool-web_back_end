bbdb395f8d6c5baa240d26e857d7ab1af329bf9b
CHRIS09MT <christophermoralestorres06@gmail.com>
2025-01-29 23:39:35 -0500

    end

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.all('*', (req, res) => {
+  res.status(404).send('<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>Error</title></head><body><pre>Cannot GET ' + req.originalUrl + '</pre></body></html>');
+});
+
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
+
+module.exports = app;
