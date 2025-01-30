49a205f070db583cbc0dacfb79c6d0380daace7a
CHRIS09MT <christophermoralestorres06@gmail.com>
2025-01-29 23:41:08 -0500

    correcting

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 
 app.get('/', (req, res) => {
@@ -6,7 +7,7 @@ app.get('/', (req, res) => {
 });
 
 app.all('*', (req, res) => {
-  res.status(404).send('<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>Error</title></head><body><pre>Cannot GET ' + req.originalUrl + '</pre></body></html>');
+  res.status(404).send(`<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>Error</title></head><body><pre>Cannot GET ${req.originalUrl}</pre></body></html>`);
 });
 
 app.listen(1245, () => {
