f8ec9e6167cf54d0e85d1747b9238c2e806cfebc
CHRIS09MT <christophermoralestorres06@gmail.com>
2025-01-30 16:05:53 -0500

    correcting all

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,36 +1,36 @@
-import readDatabase from '../utils.js';
+import readDatabase from '../utils';
 
 class StudentsController {
-    static async getAllStudents(req, res) {
-        try {
-            const students = await readDatabase(process.argv[2]);
-            let response = 'This is the list of our students\n';
-            const fields = Object.keys(students).sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' }));
-            
-            fields.forEach(field => {
-                response += `Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}\n`;
-            });
-            res.status(200).send(response.trim());
-        } catch (error) {
-            res.status(500).send('Cannot load the database');
-        }
-    }
+  static async getAllStudents(req, res) {
+    try {
+      const students = await readDatabase(process.argv[2]);
+      let response = 'This is the list of our students\n';
+      const fields = Object.keys(students).sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' }));
 
-    static async getAllStudentsByMajor(req, res) {
-        const { major } = req.params;
-        if (major !== 'CS' && major !== 'SWE') {
-            return res.status(500).send('Major parameter must be CS or SWE');
-        }
-        try {
-            const students = await readDatabase(process.argv[2]);
-            if (!students[major]) {
-                return res.status(200).send('List: ');
-            }
-            res.status(200).send(`List: ${students[major].join(', ')}`);
-        } catch (error) {
-            res.status(500).send('Cannot load the database');
-        }
+      fields.forEach((field) => {
+        response += `Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}\n`;
+      });
+      return res.status(200).send(response.trim());
+    } catch (error) {
+      return res.status(500).send('Cannot load the database');
     }
+  }
+
+  static async getAllStudentsByMajor(req, res) {
+    const { major } = req.params;
+    if (major !== 'CS' && major !== 'SWE') {
+      return res.status(500).send('Major parameter must be CS or SWE');
+    }
+    try {
+      const students = await readDatabase(process.argv[2]);
+      if (!students[major]) {
+        return res.status(200).send('List: ');
+      }
+      return res.status(200).send(`List: ${students[major].join(', ')}`);
+    } catch (error) {
+      return res.status(500).send('Cannot load the database');
+    }
+  }
 }
 
 export default StudentsController;
--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,22 +1,22 @@
 import fs from 'fs/promises';
 
 async function readDatabase(filePath) {
-    try {
-        const data = await fs.readFile(filePath, 'utf8');
-        const lines = data.trim().split('\n');
-        const students = {};
-        
-        for (let i = 1; i < lines.length; i++) {
-            const [firstname, , , field] = lines[i].split(',');
-            if (!students[field]) {
-                students[field] = [];
-            }
-            students[field].push(firstname);
-        }
-        return students;
-    } catch (error) {
-        throw new Error('Cannot load the database');
+  try {
+    const data = await fs.readFile(filePath, 'utf8');
+    const lines = data.trim().split('\n');
+    const students = {};
+
+    for (let i = 1; i < lines.length; i += 1) {
+      const [firstname, , , field] = lines[i].split(',');
+      if (!students[field]) {
+        students[field] = [];
+      }
+      students[field].push(firstname);
     }
+    return students;
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 export default readDatabase;
