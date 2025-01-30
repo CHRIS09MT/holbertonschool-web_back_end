04f0dfa8113a83c3ead0b9d609ff2ed390cfe671
CHRIS09MT <christophermoralestorres06@gmail.com>
2025-01-29 19:49:51 -0500

    ready?

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,10 @@
-console.log("Welcome to Holberton School, what is your name?\r");
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
 process.stdin.on("data", (data) => {
-	const name = data.toString().trim();
-	console.log(`Your name is: ${name}\r`);
-	console.log("This important software is now closing\r");
+    const name = data.toString().trim();
+    console.log(`Your name is: ${name}`);
+});
 
-	process.exit();
+process.on("beforeExit", () => {
+    console.log("This important software is now closing");
 });
